6ec628da0d47c57e465f41e076e21620253bee7f
lamaravilla18 <jm0162321@gmail.com>
2025-11-14 16:12:10 -0500

    agrega clases LoginRequest y LoginResponse para manejo de autenticación

---
+++ src/main/java/com/parcialtres/demo/dto/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.parcialtres.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String type = "Bearer";
+    private String email;
+    private String username;
+    private String rol;
+
+    public LoginResponse(String token, String email, String username, String rol) {
+        this.token = token;
+        this.email = email;
+        this.username = username;
+        this.rol = rol;
+    }
+}
