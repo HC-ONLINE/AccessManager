4aa89486c7a8cee61eb4042511efbd545b511c5d
HC-ONLINE <henriquezandres856@gmail.com>
2025-12-27 20:00:57 -0500

    elimina atributos y métodos innecesarios de la clase Usuario

--- src/main/java/com/proaula/nomina/model/Usuario.java
+++ src/main/java/com/proaula/nomina/model/Usuario.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "usuarios")
@@ -30,30 +29,6 @@ public class Usuario {
 
     @Column(name = "apellido", nullable = false, length = 100)
     private String apellido;
-
-    @Column(name = "documento", nullable = false, unique = true, length = 20)
-    private String documento;
-
-    @Column(name = "tipo_documento", length = 5)
-    private String tipoDocumento;
-
-    @Column(name = "fecha_nacimiento")
-    private LocalDate fechaNacimiento;
-
-    @Column(name = "genero", length = 30)
-    private String genero;
-
-    @Column(name = "telefono", length = 20)
-    private String telefono;
-
-    @Column(name = "direccion")
-    private String direccion;
-
-    @Column(name = "cargo", length = 100)
-    private String cargo;
-
-    @Column(name = "tipo_contrato", length = 30)
-    private String tipoContrato;
 
     @Column(name = "estado", length = 20)
     private String estado;
@@ -100,70 +75,6 @@ public class Usuario {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public String getTipoDocumento() {
-        return tipoDocumento;
-    }
-
-    public void setTipoDocumento(String tipoDocumento) {
-        this.tipoDocumento = tipoDocumento;
-    }
-
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public String getTipoContrato() {
-        return tipoContrato;
-    }
-
-    public void setTipoContrato(String tipoContrato) {
-        this.tipoContrato = tipoContrato;
     }
 
     public String getEstado() {
